067ffebadfb67ecd1bff548987f994fda8bcc157
WakeUp-Jin <2385472291@qq.com>
2025-10-27 22:09:19 +0800

    feat:修复config的更新中的文件按钮

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -20,20 +20,20 @@ export default {
         {
           text: '前言',
           items: [
-            { text: '上下文组成' },
+            // { text: '上下文组成' },
             { text: '更新日记', link: '/更新日记/更新日记' }
           ]
         },
         {
           text: '第一部分：上下文工程基础技术',
           items: [
-            { text: '第一章 数据持久化',  },
-            { text: '第二章 向量存储和嵌入模型',  },
-            { text: '第三章 知识图谱',  },
+            { text: '第一章 数据持久化(待整理)',  },
+            { text: '第二章 向量存储和嵌入模型(待整理)',  },
+            { text: '第三章 知识图谱(待整理)',  },
             { text: '第四章 RAG技术', 
               items: [
                 { text: 'RAG策略', link: '/RAG技术/RAG策略-index' },
-                { text: '编码器和LLM微调'}
+                // { text: '编码器和LLM微调'}
               ]
             },
             { text: '第五章 搜索代理', link: '/搜索代理/搜索代理' }
@@ -42,16 +42,16 @@ export default {
         {
           text: '第二部分：上下文组成工程实践',
           items: [
-            { text: '第五章 系统提示词模块',  },
+            { text: '第五章 系统提示词模块(待整理)',  },
             { text: '第六章 工具管理模块',
               items: [
                 { text: '工具管理', link: '/工具管理模块/工具管理' },
                 { text: 'ClaudeCode逆向工程（Kode）的工具定义和管理', link: '/工具管理模块/ClaudeCode逆向工程（Kode）的工具定义和管理 -TS版本' },
-                { text: '模型调用工具能力评估',  },
-                { text: 'MCP与MCP的安全',  }
+                // { text: '模型调用工具能力评估',  },
+                // { text: 'MCP与MCP的安全',  }
               ]
             },
-            { text: '第七章 用户记忆模块',  },
+            { text: '第七章 用户记忆模块(待整理)',  },
             { text: '第八章 会话存储模块',
               items: [
                 { text: 'Redis缓存后端存储设计-读穿｜写穿', link: '/会话存储模块/Redis缓存后端存储设计-读穿｜写穿' },
@@ -64,12 +64,12 @@ export default {
                 { text: 'LLM输出格式成本：为什么JSON比TSV成本更高', link: '/结构化输出模块/LLM 输出格式成本：为什么 JSON 比 TSV 成本更高' }
               ]
             },
-            { text: '第十章 相关上下文模块' },
+            { text: '第十章 相关上下文模块(待整理)',  },
             { text: '第十一章 LLM模块',
               items: [
                 { text: 'LLM服务层的实现设计', link: '/LLM模块/LLM服务层的实现设计' },
                 { text: 'Cipher的LLM 服务架构分析文档', link: '/LLM模块/Cipher的LLM 服务架构分析文档 -TS版本' },
-                { text: '三大LLM提供商实现策略细节' }
+                // { text: '三大LLM提供商实现策略细节' }
               ]
             }
           ]
